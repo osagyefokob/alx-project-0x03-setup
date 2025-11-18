1b92aa3edcbf88c32408fcd6d603b42057eb255b
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-18 08:30:15 +0000

    Update Layout.tsx

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,6 +1,18 @@
-import { ReactNode } from "react";
+import { LayoutProps } from "@/interface";
 import Footer from "./Footer";
 import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
 
 interface LayoutProps {
   children: ReactNode;
